5e1282a95080605bb7248ab7319536b0c9d05016
Toni Tienda <tonitienda@gmail.com>
2025-04-24 12:09:12 +0200

    feat: dirty implementation to generate html pages from markdown posts

--- cmd/main.go
+++ cmd/main.go
@@ -2,140 +2,238 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"fmt"
-	"io"
+	"html/template"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/cobra"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 	"github.com/yuin/goldmark/text"
+	"gopkg.in/yaml.v3"
 )
 
+var (
+	watchMode     bool
+	sseClients    = make(map[chan string]bool)
+	sseRegister   = make(chan chan string)
+	sseUnregister = make(chan chan string)
+)
+
+type PostMeta struct {
+	Slug     string   `yaml:"slug"`
+	Title    string   `yaml:"title"`
+	Date     string   `yaml:"date"`
+	Tags     []string `yaml:"tags"`
+	Markdown string   `yaml:"markdown"`
+}
+
+type SiteData struct {
+	Title string
+	HTML  template.HTML
+	Watch bool
+}
+
+//go:embed templates/layout.html
+var layoutHTML string
+
 func main() {
-	fmt.Println("Hello, World!")
-
-	// Read the file passed as first argument
-	filename := os.Args[1]
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-	// Read the file content
-	content, err := io.ReadAll(file)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-	// Print the file content
-	//fmt.Println(string(content))
-
-	// Get AST from goldmark
-	md := goldmark.New()
-	reader := text.NewReader(content)
-
-	doc := md.Parser().Parse(reader)
-	if doc == nil {
-		fmt.Println("Error parsing document")
-		return
-	}
-	// Print the AST
-	fmt.Println("AST:")
-	printAST(doc, content, 0)
-
-	html := toHTML(doc, content)
-
-	fmt.Println("HTML:")
-	fmt.Println(html)
-
-	// Write html to the file passed as second argument
-	if len(os.Args) > 2 {
-		htmlFile, err := os.Create(os.Args[2])
-		if err != nil {
-			fmt.Println("Error creating file:", err)
-			return
-		}
-		defer htmlFile.Close()
-		// Write the HTML content to the file
-		_, err = htmlFile.WriteString(html)
-		if err != nil {
-			fmt.Println("Error writing file:", err)
-			return
-		}
-		fmt.Println("HTML written to", os.Args[2])
-	} else {
-		fmt.Println("No output file specified")
+	var postsPath string
+	var outputPath string
+	var rootCmd = &cobra.Command{
+		Use:   "mdsite",
+		Short: "Static site generator from Markdown",
+		Run: func(cmd *cobra.Command, args []string) {
+			generateSite(postsPath, outputPath)
+			if watchMode {
+				go startSSEServer()
+				go serveOutput(outputPath)
+				watchAndRebuild(postsPath, outputPath)
+				select {}
+			}
+		},
 	}
 
+	rootCmd.Flags().StringVar(&postsPath, "posts", "posts.yaml", "Path to posts.yaml")
+	rootCmd.Flags().StringVar(&outputPath, "output", "output", "Output directory")
+	rootCmd.Flags().BoolVar(&watchMode, "watch", false, "Enable watch mode with hot reload")
+	rootCmd.Execute()
 }
 
-func toHTML(n ast.Node, content []byte) string {
-	html := ""
-	for c := n.FirstChild(); c != nil; c = c.NextSibling() {
-
-		language := ""
-		level := 0
-
-		if node, ok := c.(*ast.FencedCodeBlock); ok {
-			language = string(node.Language(content))
-			fmt.Printf("%s (%s)", c.Kind().String(), language)
-			html += fmt.Sprintf("<pre><code class=\"language-%s\">\n", language)
-			html += string(node.Text(content))
-			html += "</code></pre>\n"
-
-		} else if node, ok := c.(*ast.Heading); ok {
-			level = node.Level
-			html += fmt.Sprintf("<h%d>%s</h%d>\n", level, c.Text(content), level)
-		}
-
-		// if !c.HasChildren() {
-		// 	segment := c.Text(content)
-		// 	if len(segment) > 0 {
-		// 		fmt.Printf(": %q", segment)
-		// 	}
-		// }
-		// fmt.Println()
-		// fmt.Println()
-
-		html += toHTML(c, content)
+func generateSite(postsPath, outputPath string) {
+	data, err := os.ReadFile(postsPath)
+	if err != nil {
+		log.Fatalf("failed to read posts file: %v", err)
+	}
+	var posts []PostMeta
+	if err := yaml.Unmarshal(data, &posts); err != nil {
+		log.Fatalf("failed to parse YAML: %v", err)
 	}
 
-	return html
+	fmt.Println("POSTS:", posts)
 
-}
+	md := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(parser.WithAutoHeadingID()),
+		goldmark.WithRendererOptions(html.WithHardWraps(), html.WithXHTML()),
+	)
+	tmpl := template.Must(template.New("layout").Parse(layoutHTML))
 
-func printAST(n ast.Node, content []byte, indent int) {
-	for c := n.FirstChild(); c != nil; c = c.NextSibling() {
-
-		language := ""
-		level := 0
-
-		if node, ok := c.(*ast.FencedCodeBlock); ok {
-			language = string(node.Language(content))
-			fmt.Printf("%s%s (%s)", spaces(indent), c.Kind().String(), language)
-
-		} else if node, ok := c.(*ast.Heading); ok {
-			level = node.Level
-			fmt.Printf("%s%s (%d)", spaces(indent), c.Kind().String(), level)
-		} else {
-
-			fmt.Printf("%s%s", spaces(indent), c.Kind().String())
+	os.MkdirAll(outputPath, 0755)
+	for _, post := range posts {
+		content, err := os.ReadFile(post.Markdown)
+		if err != nil {
+			log.Printf("failed to read %s: %v", post.Markdown, err)
+			continue
 		}
+		var buf bytes.Buffer
+		context := parser.NewContext()
+		doc := md.Parser().Parse(text.NewReader(content), parser.WithContext(context))
+		printAST(doc, content)
 
-		if !c.HasChildren() {
-			segment := c.Text(content)
-			if len(segment) > 0 {
-				fmt.Printf(": %q", segment)
+		title := post.Title
+		if title == "" {
+			if h1 := findFirstH1(doc, content); h1 != "" {
+				title = h1
 			}
 		}
-		fmt.Println()
-		fmt.Println()
+		if post.Slug == "" {
+			post.Slug = slugify(title)
+		}
 
-		printAST(c, content, indent+2)
+		md.Renderer().Render(&buf, content, doc)
+
+		outFile := filepath.Join(outputPath, post.Slug+".html")
+		f, err := os.Create(outFile)
+		if err != nil {
+			log.Printf("failed to create output file: %v", err)
+			continue
+		}
+
+		fmt.Println("Generated html:", buf.String())
+		tmpl.Execute(f, SiteData{
+			Title: title,
+			HTML:  template.HTML(buf.String()),
+			Watch: watchMode,
+		})
+		f.Close()
 	}
 }
 
-func spaces(n int) string {
-	return string(bytes.Repeat([]byte(" "), n))
+func printAST(node ast.Node, source []byte) {
+	for n := node.FirstChild(); n != nil; n = n.NextSibling() {
+		switch n := n.(type) {
+		case *ast.Heading:
+			fmt.Printf("\nHeading: %d %s\n", n.Level, n.Text(source))
+		case *ast.FencedCodeBlock:
+			fmt.Printf("\nCode Block: %s \n%s\n\n", (*ast.FencedCodeBlock)(n).Language(source), n.Lines().Value(source))
+		case *ast.Paragraph:
+			fmt.Printf("\nParagraph: %s \n", (*ast.Paragraph)(n).Lines().Value(source))
+		case *ast.Text:
+			fmt.Printf("\nText: %s \n", (*ast.Text)(n).Value(source))
+		case *ast.HTMLBlock:
+			fmt.Printf("\nHTML Block: %s\n", n.Text(source))
+		default:
+			fmt.Printf("\nNode: %T\n", n)
+		}
+		printAST(n, source)
+	}
+}
+
+func findFirstH1(doc ast.Node, source []byte) string {
+	for n := doc.FirstChild(); n != nil; n = n.NextSibling() {
+		if h, ok := n.(*ast.Heading); ok && h.Level == 1 {
+			return string(h.Text(source))
+		}
+	}
+	return ""
+}
+
+func slugify(s string) string {
+	s = strings.ToLower(s)
+	s = strings.ReplaceAll(s, " ", "-")
+	s = strings.ReplaceAll(s, ".", "")
+	s = strings.ReplaceAll(s, "/", "")
+	return s
+}
+
+func watchAndRebuild(postsPath, outputPath string) {
+	w, _ := fsnotify.NewWatcher()
+	defer w.Close()
+	//contentDir := filepath.Dir(postsPath)
+	w.Add(postsPath)
+	w.Add("templates")
+	filepath.WalkDir("content", func(path string, d fs.DirEntry, err error) error {
+		if !d.IsDir() && strings.HasSuffix(path, ".md") {
+			w.Add(path)
+		}
+		return nil
+	})
+	for {
+		select {
+		case <-w.Events:
+			generateSite(postsPath, outputPath)
+			broadcastReload()
+		case err := <-w.Errors:
+			log.Println("watch error:", err)
+		}
+	}
+}
+
+func serveOutput(outputPath string) {
+	http.Handle("/", http.FileServer(http.Dir(outputPath)))
+	http.HandleFunc("/reload", handleSSE)
+	log.Println("Serving on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleSSE(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	client := make(chan string)
+	sseRegister <- client
+	defer func() { sseUnregister <- client }()
+	for {
+		select {
+		case <-client:
+			fmt.Fprintf(w, "data: reload\n\n")
+			flusher.Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
+}
+
+func startSSEServer() {
+	for {
+		select {
+		case c := <-sseRegister:
+			sseClients[c] = true
+		case c := <-sseUnregister:
+			delete(sseClients, c)
+			close(c)
+		}
+	}
+}
+
+func broadcastReload() {
+	for c := range sseClients {
+		c <- "reload"
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module blog-down
 
 go 1.24.0
 
-require github.com/yuin/goldmark v1.7.10 // indirect
+require (
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.9.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/yuin/goldmark v1.7.10 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
